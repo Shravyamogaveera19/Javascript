776d491e1d2bc6e4681203b600f00b27a1e3f29f
Shravya S Mogaveera <shravyasmogaveera@gmail.com>
2025-02-23 12:29:19 +0530

    Importing and Exporting modules and Use of top level await

---
+++ ModernScript/shoppingCart.js
@@ -0,0 +1,24 @@
+//Exporting module
+console.log("Exporting module");
+
+// console.log('Start Fetching Users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finished Fetching Users');
+
+const shippingCost = 10;
+export const cart = [];
+
+export const addToCart = function(product, quantity){
+    cart.push({product,quantity});
+    console.log(`${quantity} ${product} added to cart`);
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export {totalPrice,totalQuantity as qt};
+
+export default function(product, quantity){
+    cart.push({product,quantity});
+    console.log(`${quantity} ${product} added to cart`);
+};
